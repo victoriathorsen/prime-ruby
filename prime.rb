38a3845c62335ff59d6daece6f340e5bc6f6ebd1
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-13 23:39:18 -0800

    commit tree 6def590a24603a5955151a3567018ad4de1373dd

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(prime)
+    if prime <= 1
+        return false
+    else (2..prime - 1).each do |i|
+        if prime % i == 0   
+        return false
+      end
+    end
+end
+    true
+end
